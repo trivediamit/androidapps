72288dc9dcdb3bc51f36f72f6491241fe9fc16eb
Amit Trivedi <amittrivedi.work@gmail.com>
2015-04-14 23:44:07 +0530

    Toolbar code set in Activity Splash Activity added

--- app/src/main/java/com/myprojects/android/tipsy/Home.java
+++ app/src/main/java/com/myprojects/android/tipsy/Home.java
@@ -2,16 +2,20 @@ package com.myprojects.android.tipsy;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class Home extends ActionBarActivity {
 
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mToolbar = (Toolbar) findViewById(R.id.app_toolbar);
+        setSupportActionBar(mToolbar);
     }
 
 
